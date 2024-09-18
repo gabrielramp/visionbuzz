41628e8dee5c3ce9fe5d52320141b52f20c71b40
Ivy Zhang <zivy042003@gmail.com>
2024-09-18 15:10:45 -0400

    Add simple commands to setup database tables

---
+++ backend/db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    uid         serial PRIMARY KEY,
+    username    varchar NOT NULL,
+    pwd         varchar NOT NULL
+);
+
+CREATE TABLE contacts (
+    cid         serial PRIMARY KEY,
+    uid         integer REFERENCES users(uid),        -- TODO: Check foreign keys
+    name        varchar NOT NULL,
+    vib_pattern integer,           -- TODO: design how this works!
+    temp        integer NOT NULL,
+    last_seen   timestamptz
+);
+
+CREATE TABLE embeddings (
+    cid integer REFERENCES contacts(cid),
+    uid integer REFERENCES users(uid),
+    embedding int[][],                  -- TODO: We haven't figured out how to store embeddings
+    PRIMARY KEY (cid, uid)
+)
