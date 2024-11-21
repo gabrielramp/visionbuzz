1b32fabd6a5023614991800aeb98b3cc975c0072
CJ Cosby <charlescjcosby@gmail.com>
2024-11-20 19:41:28 -0500

    Cleaned up contacts

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'themes.dart' as themes;
+import 'dart:math';
 
 class ContactsPage extends StatelessWidget {
   ContactsPage({super.key});
@@ -34,9 +35,13 @@ class ContactsPage extends StatelessWidget {
     '?'
   ];
 
+  final String customizableLeadingString = "Whatever you want really";
+  final int maxContacts = 10;
+
   @override
   Widget build(BuildContext context) {
-    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    ThemeData themey = Theme.of(context);
+    ColorScheme colorScheme = themey.colorScheme;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contacts'),
@@ -50,10 +55,12 @@ class ContactsPage extends StatelessWidget {
   }
 
   Widget alphabetSections(BuildContext context) {
-    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    ThemeData themey = Theme.of(context);
+    ColorScheme colorScheme = themey.colorScheme;
+    TextTheme textTheme = themey.textTheme;
 
-    return Expanded(
-        child: ListView.separated(
+    return ListView.separated(
+      shrinkWrap: true,
       padding: const EdgeInsets.all(8),
       itemCount: 27,
       itemBuilder: (BuildContext context, int index) {
@@ -63,49 +70,76 @@ class ContactsPage extends StatelessWidget {
             children: [
               Align(
                 alignment: Alignment.topLeft,
-                child: Text(letterHeads[index]),
+                child: Text(
+                  letterHeads[index],
+                  style: textTheme.headlineMedium,
+                ),
               ),
-              letterContacts(context, 1, letterHeads[index])
+              letterContacts(context, Random().nextInt(maxContacts - 1) + 1,
+                  letterHeads[index])
             ]);
       },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-    ));
+      separatorBuilder: (BuildContext context, int index) =>
+          Divider(color: colorScheme.primary, thickness: 1, height: 2),
+    );
   }
 
   Widget letterContacts(
       BuildContext context, int contactsInSection, String startsWith) {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
-
-    return SizedBox(
-        height: 100,
-        child: ListView.separated(
-          padding: const EdgeInsets.all(8),
-          itemCount: contactsInSection,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-                color: Colors.blue,
-                height: 100,
-                // width: MediaQuery.of(context).size.width,
-                child: FractionallySizedBox(
-                  heightFactor: 1,
-                  widthFactor: 1,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Icon(Icons.person,
-                            size: 100, color: colorScheme.secondary),
-                        Text("Contact Name Starting with ${startsWith} Here")
-                      ],
-                    ),
-                  ),
-                ));
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ));
+    return ListView.separated(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(8),
+      itemCount: contactsInSection,
+      itemBuilder: (BuildContext context, int index) {
+        return contactRow(context,
+            "${startsWith} Contact #${index + 1} / ${customizableLeadingString}");
+      },
+      separatorBuilder: (BuildContext context, int index) => Divider(
+        color: colorScheme.primary,
+      ),
+    );
   }
 
+  Widget contactRow(BuildContext context, String name) {
+    ThemeData themey = Theme.of(context);
+    ColorScheme colorScheme = themey.colorScheme;
+    TextTheme textTheme = themey.textTheme;
+    return Container(
+        // color: Colors.red,
+        height: 100,
+        // width: MediaQuery.of(context).size.width,
+        child: FractionallySizedBox(
+          heightFactor: 1,
+          widthFactor: 1,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                Container(
+                    height: 100,
+                    margin: EdgeInsets.only(right:20),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                    child: Container(
+                      padding:EdgeInsets.symmetric(horizontal: 10),
+                        child: Icon(
+                      Icons.person,
+                      size: 90,
+                      color: colorScheme.secondary,
+                    ))),
+                Text(
+                  name,
+                  style: textTheme.headlineSmall,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
   // Widget contactRow(BuildContext context, String name) {
   //   ColorScheme colorScheme = Theme.of(context).colorScheme;
   //   return SizedBox(
--- lib/themes.dart
+++ lib/themes.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Themes {
-
-  static Color _mainColor =new Color(0xff4f009e);
+  static Color _mainColor = new Color(0xff4f009e);
   static ThemeData main = ThemeData(
     useMaterial3: true,
     appBarTheme: AppBarTheme(
-      titleTextStyle: TextStyle(
-        fontSize:30,
-        color: _mainColor,
-      )
+        titleTextStyle: TextStyle(
+      fontSize: 45,
+      color: _mainColor,
+    )),
+    textTheme: TextTheme(
+      headlineMedium: TextStyle(
+          fontSize: 35, fontWeight: FontWeight.w700, color: _mainColor),
+      headlineSmall: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
     ),
     colorScheme: ColorScheme.fromSeed(
         primary: _mainColor,
