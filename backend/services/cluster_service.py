1848356b082c19480e5f45d4adce735fdc6c95b1
Ivy Zhang <zivy042003@gmail.com>
2025-01-16 13:24:01 -0500

    created skeleton for cluster

---
+++ backend/services/cluster_service.py
@@ -0,0 +1,14 @@
+import numpy as np
+from typing import List
+
+
+class ClusterService:
+    def __init__(self, config):
+        pass
+
+    def get_clusters(embeds: np.array) -> List[int] | np.array:
+        """
+        Takes in all embeds, returns cluster ID for each one in order
+        TODO: Remove typing this is stupid
+        """
+        pass
